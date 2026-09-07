d1fe8bdefa6c1217a94b5041c82c4f9f9d60d4d5
Redwinter666 <redwinter2404@gmail.com>
2026-09-07 15:37:40 +0700

    Commit praktikum 4

--- FPW_Test1/routes/web.php
+++ FPW_Test1/routes/web.php
@@ -2,18 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
 
 // Rute bawaan halaman depan
 Route::get('/', function () {
     return view('welcome');
 });
 
-// Kode Praktikum: Rute Dashboard
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->middleware(['auth'])
     ->name('dashboard');
 
-// Kode Latihan 2: Rute /about tanpa Controller
 Route::get('/about', function () {
     return 'Ini adalah profil POS Barokah Mart';
 });
+
+Route::get('/login', [LoginController::class, 'create'])
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/login', [LoginController::class, 'store'])
+    ->middleware('guest')
+    ->name('login.store');
+
+Route::post('/logout', [LoginController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::resource('categories', CategoryController::class);
+    Route::resource('products', ProductController::class);
+    Route::get('/reports/sales', [ReportController::class, 'sales'])->name('report.sales');
+});
+
+Route::middleware(['auth', 'role:admin,kasir'])->group(function () {
+    Route::get('/pos', [PosController::class, 'index'])->name('pos.index');
+    Route::post('/pos', [PosController::class, 'store'])->name('pos.store');
+});
+
